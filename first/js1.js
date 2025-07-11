a85ce984ab003d564abd2ff6ebdab48e2f00462c
Mallikarjun-P-23 <mallikarjunparoji23@gmail.com>
2025-07-11 03:56:56 +0000

    j

--- first/js1.js
+++ first/js1.js
@@ -1,1 +1,65 @@
-console.log("hello")
+console.log("Hello, Node.js!");
+
+//get input. from the terminal 
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+r1.question('Enter your name: ', (name) => {
+  console.log(`Hello, ${name}!`);
+  r1.close();
+});
+
+//input from browser
+  let name = prompt("What's your name?");
+  alert("Hello, " + name + "!");
+
+
+
+const readline = require('readline');
+
+
+const r2 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+r2.question("What's your name? ", function(name) {
+  console.log(`Hello, ${name}!`);
+  r2.close();
+});
+
+
+const a=[1,2,3,4,5]
+for(const n of a)
+{
+    console.log(n)
+}
+
+const b=a.map((n)=>n*2)
+console.log(b)
+
+
+const n1 = new Map();
+n1.set('1',1)
+n1.set('a',2)
+console.log(n1.get('a'))
+for(const [k,v] of n1)
+{
+    console.log(k,v)
+}
+
+
+const o={
+    g1:"Nfs",
+    g2:"Nf2"
+}
+for(const a in o)
+{
+    console.log(a,0[a])
+}
+
+
